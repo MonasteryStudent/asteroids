ce5e4273fad57f5a7032a5e4dd6bc8e6395b36c4
MonasteryStudent <cs_knoll@proton.me>
2025-06-17 16:00:19 +0200

    add display screen with black color and make the quit button work

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    pygame.init()
+    surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    color = (0, 0, 0)
+    surface.fill(color)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
