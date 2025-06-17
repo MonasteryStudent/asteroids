13d381d3c11d28ef4f7f3ab90d2a97d1ec300aee
MonasteryStudent <cs_knoll@proton.me>
2025-06-17 16:20:48 +0200

    reduce game to 60 fps

--- main.py
+++ main.py
@@ -9,11 +9,14 @@ def main():
     surface = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     color = (0, 0, 0)
     surface.fill(color)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
